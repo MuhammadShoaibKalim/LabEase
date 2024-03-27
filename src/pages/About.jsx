2fad65b56139addda7dc6a8a664621ffeacf5aae
Muhammad Shoaib Kalim <mmuhammadshoaibkalim@gmail.com>
2024-03-27 23:08:14 +0500

    Updates in About and Contact pages.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,35 +1,31 @@
 import React from 'react';
-import  './about.css';
 
 const About = () => {
   return (
-    <div className='about-div-1'>
-      <h2>
-        This is h2 page here you go. 
-      </h2>
-      <p>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-     Libero odit modi voluptate nesciunt! Vel repellat ea maxime qui deserunt officia voluptas, 
-     fugit veritatis ipsa aperiam exercitationem. Perferendis mollitia alias vel?
-      </p>
-      <h2>
-        This is h2 page here you go. 
-      </h2>
-      <p>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-     Libero odit modi voluptate nesciunt! Vel repellat ea maxime qui deserunt officia voluptas, 
-     fugit veritatis ipsa aperiam exercitationem. Perferendis mollitia alias vel?
-      </p>
-      <h2>
-        This is h2 page here you go. 
-      </h2>
-      <p>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-     Libero odit modi voluptate nesciunt! Vel repellat ea maxime qui deserunt officia voluptas, 
-     fugit veritatis ipsa aperiam exercitationem. Perferendis mollitia alias vel?
-      </p>
+    <div className="container" style={{marginTop:120}}>
+      <div className="row justify-content-center">
+        <div className="col-lg-8 col-md-10">
+          <p className="text-center mb-5" style={{ color: "#75dbd0", fontWeight: "bold", fontSize: 22 }}>
+            INTRODUCING
+          </p>
+          <h4 className="text-center mb-5" style={{fontFamily:"cursive",fontSize:28,fontWeight:"bold"}}> LabEase Redefining HealthCare Access</h4>
+          <p className="text-center">
+            LabEase is a comprehensive web-based platform designed to streamline access to laboratory services for patients. It simplifies the process of finding, comparing, and scheduling lab tests. The platform centralizes information on tests, prices, appointments, and results, making it easier for users to make informed healthcare decisions. With modules like test information, appointment scheduling, result display, and user feedback, LabEase aims to enhance the patient experience, save time, and improve the overall efficiency of healthcare services.
+          </p>
+          <ul>
+            <li>Centralize Information: Provide a single platform where patients can access comprehensive information about various lab tests, including prices, descriptions, and instructions.</li>
+            <li>Facilitate Comparison: Enable users to compare prices among different laboratories and make informed decisions regarding their healthcare needs.</li>
+            <li>Streamline Appointments: Offer an easy-to-use appointment module, allowing patients to schedule tests hassle-free.</li>
+            <li>Efficient Result Delivery: Display test results directly on the platform once processed, ensuring timely access for patients.</li>
+            <li>Enhance User Experience: Implement modules for user feedback, testimonials, and notifications to continuously improve the system and build trust among users.</li>
+            <li>Streamline Appointments: Offer an easy-to-use appointment module, allowing patients to schedule tests hassle-free.</li>
+            <li>Efficient Result Delivery: Display test results directly on the platform once processed, ensuring timely access for patients.</li>
+            <li>Enhance User Experience: Implement modules for user feedback, testimonials, and notifications to continuously improve the system and build trust among users.</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,51 @@
-import React from 'react'
-import './contact.css';
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
 
 const Contact = () => {
   return (
-    <div className='contact-div-1'>
-      <h2>Hello this is contact page.</h2>
+    <div className="container justify-content-center align-items-center vh-100" style={{ marginTop: 80 }}>
+      <div className="row justify-content-center align-items-center">
+        <div className="col-md-8 col-lg-6">
+          <p className="text-center sm: mt-5" style={{ color: "#75dbd0", fontWeight: "bold", fontSize: 34, fontFamily: "cursive" }}>Contact Support</p>
+          <p className="text-center mb-4">We're always excited to discuss your needs. Get in touch with us and we'll get back to you as soon as possible.</p>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio ipsa dolorem quidem cum corrupti aut nisi nihil dicta a optio laudantium voluptatibus nam eos quisquam perspiciatis labore accusantium assumenda ipsam ducimus ea, harum minima similique? Magni, possimus rerum voluptatibus perferendis doloremque a autem, neque ratione velit, officia cum soluta.
-      </p>
+          <Form>
+            <div className="row">
+              <div className="col">
+                <Form.Group controlId="firstName">
+                  <Form.Label>First Name</Form.Label>
+                  <Form.Control type="text" />
+                </Form.Group>
+              </div>
+              <div className="col">
+                <Form.Group controlId="lastName">
+                  <Form.Label>Last Name</Form.Label>
+                  <Form.Control type="text"   />
+                </Form.Group>
+              </div>
+            </div>
+
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email"   />
+            </Form.Group>
+
+            <Form.Group controlId="phone">
+              <Form.Label>Phone No</Form.Label>
+              <Form.Control type="tel"   />
+            </Form.Group>
+
+            <Form.Group controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control as="textarea" rows={3}   />
+            </Form.Group>
+
+            <Button variant="primary" type="let's talk" className="w-100 mt-5 p-2" style={{backgroundColor:"gray"}}>let's talk</Button>
+          </Form>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
